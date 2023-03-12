a1391cbdc55ace9af9607fbe135448af07578310
Khaled <khaledabuhelal@gmail.com>
2023-03-13 00:35:24 +0300

    hcg

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 // Import React and ReactDom libraries
-import React from "react";
-import { ReactDOM } from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 // Get a reference to the div with id root
-const els = document.getElementById('root');
+const el = document.getElementById('root');
 // tell React to take control of the element
-const root = ReactDOM.createroot(els);
+const root = ReactDOM.createRoot(el);
 // create a component
 function App() {
-    return <h1>Hi there!</h1>;
+    const inputType = 'number';
+    const minValue = 5;
+    const maxValue = 10;
+
+
+    return <input type={inputType} min={minValue} max={maxValue} />;
 }
 // show the component on the screen
-root.render(<App/>)
+root.render(<App />)
