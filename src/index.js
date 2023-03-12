ea999faa0de4f73e542ccc6b574cbbbae353b369
Khaled <khaledabuhelal@gmail.com>
2023-03-12 23:34:51 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Import React and ReactDom libraries
+import React from "react";
+import { ReactDOM } from "react";
+// Get a reference to the div with id root
+const el = document.getElementById('root');
+// tell React to take control of the element
+const root = ReactDOM.createroot(el);
+// create a component
+function App() {
+    return <h1>Hi there!</h1>;
+}
+// show the component on the screen
+root.render(<App/>)
