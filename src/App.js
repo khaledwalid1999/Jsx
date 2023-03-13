883b1264a6f464cd6acb3b07eeccaf255c5adf4b
Khaled <khaledabuhelal@gmail.com>
2023-03-14 00:36:31 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,5 @@
+function App() {
+      return <input style = {{border: "1px solid blue"}}/>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 // Import React and ReactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 // Get a reference to the div with id root
 const el = document.getElementById('root');
 // tell React to take control of the element
 const root = ReactDOM.createRoot(el);
 // create a component
-function App() {
-    const inputType = 'number';
-    const minValue = 5;
-    const maxValue = 10;
 
-
-    return <input type={inputType} min={minValue} max={maxValue} />;
-}
 // show the component on the screen
 root.render(<App />)
